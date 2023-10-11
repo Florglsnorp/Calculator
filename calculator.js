4b46b168a5dafa4eb3a94c6540a1a9051f9b53b9
CEller03 <coleton.eller@gmail.com>
2023-10-11 10:22:06 -0400

    added calculate function

---
+++ calculator.js
@@ -0,0 +1,12 @@
+function calculate(num1, num2, operator){
+    var result = 0
+    if(operator == "+"){
+        result = num1 + num2
+    }else if(operator == "-"){
+        result = num1 - num2
+    }else if(operator == "*"){
+        result = num1 * num2
+    }else{
+        result = num1 / num2
+    }
+}
